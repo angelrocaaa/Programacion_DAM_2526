857f29e2e8529141bb977bbc436076da1e2b477d
A720\dam1 <rocamora29angel@gmail.com>
2025-09-25 19:06:07 +0200

    Actividad. Mi primer proyecto de desarrollo. Crear y ejecutar un programa simple en Java con IntelliJ IDEA

---
+++ Ejercicios_programacion_dam_2526/src/main/java/org/example/Actividad.java
@@ -0,0 +1,33 @@
+package org.example;
+import java.util.Scanner;
+
+public class Actividad {
+
+    static void main() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("¿Cuál es tu nombre? ");
+        String nombre = teclado.nextLine();
+
+        System.out.println("¿A qué curso vas? ");
+        String curso = teclado.next();
+
+        System.out.println("Hola " + nombre + " bienvenid@ al curso " + curso);
+
+        System.out.println("Introduce un número...");
+        int num1 = teclado.nextInt();
+
+        System.out.println("Introduce otro número...");
+        int num2 = teclado.nextInt();
+
+        // opción 1
+        System.out.println("El resultado es " + (num1+num2));
+
+        // opción 2
+        int resultado = num1 + num2;
+        System.out.println("El resultado es " + resultado);
+
+
+    }
+}
