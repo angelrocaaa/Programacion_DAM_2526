c936347f8e67366e9d1af2d63dad669e85d49300
angelrocaaa <rocamora29angel@gmail.com>
2025-10-23 14:42:18 +0200

    Practica2UD2.java

--- Ejercicios_programacion_dam_2526/src/main/java/org/example/Practica2UD2.java
+++ Ejercicios_programacion_dam_2526/src/main/java/org/example/Practica2UD2.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Practica2UD2 {
     static void main() {
 
+        //Declaramos las variables
         Scanner teclado = new Scanner(System.in);
         double resultado = 0;
         double operando1 = 0;
         double operando2 = 0;
 
+        //Bienvenida
         System.out.println("**** BIENVENIDO A LA CALCULADORA RÁPIDA ****");
 
+        //Hacemos el try catch ya que le estamos pidiendo que nos escriba un número y nos puede saltar el catch si no se introduce un número
         try {
             System.out.println("Introduce el primer operando: ");
             operando1 = teclado.nextDouble();
@@ -20,6 +23,7 @@ public class Practica2UD2 {
             return;
         }
 
+        //Menú que veremos por pantalla
         System.out.println("--------------------");
         System.out.println("  [+]  ->  sumar");
         System.out.println("  [-]  ->  restar");
@@ -30,7 +34,9 @@ public class Practica2UD2 {
         System.out.println("Elige una operación: ");
         String operacion = teclado.next();
 
+        //Si la variable operación es distinto a R entonces le pediremos que introduzca el segundo operando
         if (!operacion.equals("R")) {
+            //Hacemos el try catch ya que le estamos pidiendo que nos escriba un número y nos puede saltar el catch si no se introduce un número
             try {
                 System.out.println("Introduce el segundo operando: ");
                 operando2 = teclado.nextDouble();
@@ -40,20 +46,25 @@ public class Practica2UD2 {
             }
 
         }
+        //La variable operación depende el valor que tenga hara una de las siguientes operaciones y si no es ninguna de las que hay salta el default diciendo que operación no disponible
         switch (operacion) {
             case "+":
+                //Hacemos la suma
                 resultado = operando1 + operando2;
                 System.out.println("El resultado de " + operando1 + " + " + operando2 + " es " + resultado);
                 break;
             case "-":
+                //Hacemos la resta
                 resultado = operando1 - operando2;
                 System.out.println("El resultado de " + operando1 + " - " + operando2 + " es " + resultado);
                 break;
             case "x":
+                //Hacemos la multiplicación
                 resultado = operando1 * operando2;
                 System.out.println("El resultado de " + operando1 + " * " + operando2 + " es " + resultado);
                 break;
             case "/":
+                //Hacemos la división si el operando2 no es igual a 0, si es igual la división no se hará
                 if (operando2 == 0) {
                     System.out.println("No se puede dividir entre 0");
                 } else {
@@ -62,13 +73,16 @@ public class Practica2UD2 {
                 }
                 break;
             case "R":
+                //Hacemos la raíz cuadrada si el operando 1 es mayor que 0, si no lo es no se hace la raíz cuadrada ya que no se puede hacer de números negativos
                 if (operando1 < 0) {
                     System.out.println("No se puede hacer la raíz cuadrada de números negativos");
+                } else {
+                    resultado = Math.sqrt(operando1);
+                    System.out.println("El resultado de la raíz cuadrada de " + operando1 + " es " + resultado);
                 }
-                resultado = Math.sqrt(operando1);
-                System.out.println("El resultado de la raíz cuadrada de " + operando1 + " es " + resultado);
                 break;
             default:
+                //Si no es ninguna de las opciones anteriores salta ERROR. Operación no disponible
                 System.out.println("ERROR. Operación no disponible");
                 break;
         }
