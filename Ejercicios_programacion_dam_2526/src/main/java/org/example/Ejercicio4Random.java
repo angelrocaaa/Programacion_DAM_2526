b8f872e131e2ad2fc6b6127137830afbe53f6a12
A720\dam1 <rocamora29angel@gmail.com>
2025-10-29 20:59:27 +0100

    Ejercicios Random

--- Ejercicios_programacion_dam_2526/src/main/java/org/example/Ejercicio4Random.java
+++ Ejercicios_programacion_dam_2526/src/main/java/org/example/Ejercicio4Random.java
@@ -8,17 +8,24 @@ public class Ejercicio4Random {
 
         Random aleatorio = new Random();
 
-        final int MIN = 8;
-        final int MAX = 12;
+        final int MIN = 5;
+        final int MAX = 9;
 
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         String mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String minus = "abcdefghijklmnopqrstuvwxyz";
         String num = "0123456789";
+        String contrasenya = "";
+        int rango = aleatorio.nextInt(MAX - MIN + 1) + MIN;
 
-
-
-        for (int i=0;i<12;i++) {
-
+        for (int i=0;i<rango ; i++) {
+            contrasenya += caracteres.charAt(aleatorio.nextInt(caracteres.length()-1));
         }
+
+        contrasenya += mayus.charAt(aleatorio.nextInt( mayus.length() - 1));
+        contrasenya += minus.charAt(aleatorio.nextInt( minus.length() - 1));
+        contrasenya += num.charAt(aleatorio.nextInt( num.length() - 1));
+
+        System.out.println(contrasenya);
     }
 }
