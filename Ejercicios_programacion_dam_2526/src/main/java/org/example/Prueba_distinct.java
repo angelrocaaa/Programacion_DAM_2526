cfa574cea80078bd41b9c73884cdc1cb8c8bf0cd
A720\dam1 <rocamora29angel@gmail.com>
2025-11-14 18:19:26 +0100

    Actividad. Eliminar duplicados de un array

---
+++ Ejercicios_programacion_dam_2526/src/main/java/org/example/Prueba_distinct.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Prueba_distinct {
+
+    static void main() {
+
+        int numeritos[] = {1,2,2,3,4,5,6,6,7};
+
+        int numeritos_limpios[] = Arrays.stream(numeritos).distinct().toArray();
+
+        System.out.println(Arrays.toString(numeritos_limpios));
+
+
+    }
+}
