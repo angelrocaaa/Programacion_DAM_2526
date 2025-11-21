76da9a87e5527302f2c6c24fe654554f5ee5812d
A720\dam1 <rocamora29angel@gmail.com>
2025-11-21 19:29:54 +0100

    Practica 3 UD3

---
+++ Ejercicios_programacion_dam_2526/src/main/java/org/example/Practica3_UD3.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practica3_UD3 {
+
+    static void main() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        final int MAXIMO = 48;
+        int ancho = 0;
+        int alto = 0;
+
+        System.out.println("******* VALIDADOR DE COMPATIBILIDAD ZX SPECTRUM *******");
+        System.out.println("Introduce la resolución de tu pantalla (ancho x alto)...");
+
+        while (ancho % 8 == 1 || alto % 8 == 1 || ancho > MAXIMO || alto > MAXIMO) {
+            System.out.println("Ancho:");
+            ancho = teclado.nextInt();
+            System.out.println("Alto:");
+            alto = teclado.nextInt();
+        }
+
+        String matriz[][] = new String[alto][ancho];
+
+        externo:
+        for (int i = 0; i < matriz.length; i++) {
+            interno:
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println("Introduce línea a línea) los colores de tu imagen para cada píxel:");
+                String letra = teclado.next();
+
+                if (!letra.matches("[A-O]")) {
+                    System.out.println("Has introducido un valor de píxel incorrecto (A-O)");
+                    i--;
+                    continue externo;
+                }
+            }
+        }
+
+    }
+}
